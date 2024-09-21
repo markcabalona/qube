56cca646b9c18ed10fc7ae4be2a009964db5b305
Mark Cabalona <markcabalona58@gmail.com>
2024-09-22 00:24:38 +0800

    #34: Reimplemented DeliveryDetailsPage to show list of delivery details card

--- lib/features/transactions/presentation/pages/delivery_details_page.dart
+++ lib/features/transactions/presentation/pages/delivery_details_page.dart
@@ -1,27 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
+import 'package:qube/core/enums/app_status.dart';
+import 'package:qube/core/enums/transaction_step.dart';
+import 'package:qube/core/widgets/shimmer_transaction_list_widget.dart';
 import 'package:qube/features/transactions/domain/entities/transaction.dart';
+import 'package:qube/features/transactions/presentation/bloc/transactions_bloc.dart';
+import 'package:qube/features/transactions/presentation/cubit/delivery_details_form_cubit.dart';
 import 'package:qube/features/transactions/presentation/widgets/delivery_details_form_widget.dart';
-import 'package:qube/features/transactions/presentation/widgets/due_date_chip.dart';
+import 'package:qube/features/transactions/presentation/widgets/transactions_list_builder.dart';
 
 class DeliveryDetailsPage extends StatelessWidget {
-  const DeliveryDetailsPage({
-    super.key,
-    required this.transaction,
-  });
-  final Transaction transaction;
+  const DeliveryDetailsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          DueDateChip(dueDate: transaction.dueDate),
-          const SizedBox(height: 10),
-          DeliveryDetailsFormWidget(
-            transaction: transaction,
-          ),
-        ],
+    final bloc = GetIt.instance<TransactionsBloc>(
+      instanceName: TransactionStep.stepTwo.name,
+    )..add(const LoadTransactionsEvent(isInitialLoad: true));
+    return NotificationListener<ScrollMetricsNotification>(
+      onNotification: (notification) {
+        final shouldFetchMore = notification.metrics.pixels >=
+            notification.metrics.maxScrollExtent - 100;
+
+        if (shouldFetchMore && bloc.state.shouldFetchMore) {
+          bloc.add(const LoadTransactionsEvent());
+        }
+        return true;
+      },
+      child: BlocBuilder<TransactionsBloc, TransactionsState>(
+        bloc: bloc,
+        buildWhen: (previous, current) {
+          return current.status == AppStatus.success;
+        },
+        builder: (context, state) {
+          if (state.status == AppStatus.initial) {
+            bloc.add(const LoadTransactionsEvent());
+          }
+          return switch (state.status) {
+            AppStatus.success => _buildSuccessPage(
+                context,
+                transactions: state.transactions,
+              ),
+            (_) => const ShimmerTransactionsListWidget(),
+          };
+        },
       ),
     );
+  }
+
+  _buildSuccessPage(
+    BuildContext context, {
+    required List<Transaction> transactions,
+  }) {
+    if (transactions.isEmpty) {
+      return Center(
+        child: Text(
+          'No data to be shown.',
+          style: Theme.of(context).textTheme.headlineSmall,
+        ),
+      );
+    } else {
+      return TransactionsListBuilder(
+        transactions: transactions,
+        builder: (context, transaction) => BlocProvider(
+          create: (context) => DeliveryDetailsFormCubit(transaction),
+          child: DeliveryDetailsFormWidget(
+            transaction: transaction,
+          ),
+        ),
+      );
+    }
   }
 }
