ecd1fc8a72e61ef482500711db8e4ea8974540e0
Mark Cabalona <markcabalona58@gmail.com>
2024-09-21 16:47:06 +0800

    #7: Build TransactionsList widget based on TranscationsBloc changes
    - add new Widget TransactionsListPage

--- lib/core/dependencies/transactions_blocs.dart
+++ lib/core/dependencies/transactions_blocs.dart
@@ -1,7 +1,12 @@
 import 'package:get_it/get_it.dart';
+import 'package:qube/features/transactions/presentation/bloc/transactions_bloc.dart';
 import 'package:qube/features/transactions/presentation/cubit/transaction_step_cubit.dart';
 
 void initializeTransactionsBlocs() {
+  GetIt.instance.registerLazySingleton(
+    () => TransactionsBloc(),
+  );
+
   GetIt.instance.registerLazySingleton(
     () => TransactionStepCubit(),
   );
