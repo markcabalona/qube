dc6a0be66d30485cddebb69308293c6b8e8d01e2
Mark Cabalona <markcabalona58@gmail.com>
2024-09-21 21:16:28 +0800

    #32: Convert ShimmerTransactionListWidget to a stateless widget

--- lib/core/widgets/shimmer_transaction_list_widget.dart
+++ lib/core/widgets/shimmer_transaction_list_widget.dart
@@ -2,42 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:qube/core/widgets/shimmer_due_date_chip.dart';
 import 'package:qube/core/widgets/shimmer_transaction_card.dart';
 
-class ShimmerTransactionsListWidget extends StatefulWidget {
+class ShimmerTransactionsListWidget extends StatelessWidget {
   const ShimmerTransactionsListWidget({
     super.key,
   });
-
-  @override
-  State<ShimmerTransactionsListWidget> createState() =>
-      _ShimmerTransactionsListWidgetState();
-}
-
-class _ShimmerTransactionsListWidgetState
-    extends State<ShimmerTransactionsListWidget>
-    with SingleTickerProviderStateMixin {
-  late final AnimationController controller;
-
-  @override
-  void initState() {
-    super.initState();
-
-    controller = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 1),
-    )
-      ..repeat(period: const Duration(milliseconds: 1000))
-      ..addListener(
-        () {
-          setState(() {});
-        },
-      );
-  }
-
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
