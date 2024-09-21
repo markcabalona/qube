5f934f825491128178d9e913c9068721ff4890e5
Mark Cabalona <markcabalona58@gmail.com>
2024-09-22 00:25:56 +0800

    #34: use correct TransactionsBloc in Search widget

--- lib/features/transactions/presentation/widgets/search_transaction_widget.dart
+++ lib/features/transactions/presentation/widgets/search_transaction_widget.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
+import 'package:qube/core/enums/transaction_step.dart';
 import 'package:qube/core/utils/debounce.dart';
 import 'package:qube/features/transactions/presentation/bloc/transactions_bloc.dart';
+import 'package:qube/features/transactions/presentation/cubit/transaction_step_cubit.dart';
 
 class SearchTransactionWidget extends StatefulWidget {
   const SearchTransactionWidget({
@@ -15,41 +18,55 @@ class SearchTransactionWidget extends StatefulWidget {
 
 class _SearchTransactionWidgetState extends State<SearchTransactionWidget> {
   final debounce = Debounce();
+  final TextEditingController controller = TextEditingController();
   @override
   void dispose() {
+    controller.dispose();
     debounce.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      onTapOutside: (event) {
-        FocusManager.instance.primaryFocus?.unfocus();
+    return BlocBuilder<TransactionStepCubit, TransactionStep>(
+      buildWhen: (previous, current) {
+        controller.clear();
+        debounce.cancel();
+        return previous != current;
       },
-      onChanged: (value) {
-        debounce.run(() {
-          GetIt.instance<TransactionsBloc>().add(
-            SearchTransactionsEvent(
-              searchParam: value,
+      builder: (context, state) {
+        return TextField(
+          controller: controller,
+          onTapOutside: (event) {
+            FocusManager.instance.primaryFocus?.unfocus();
+          },
+          onChanged: (value) {
+            debounce.run(() {
+              GetIt.instance<TransactionsBloc>(
+                instanceName: state.name,
+              ).add(
+                SearchTransactionsEvent(
+                  searchParam: value,
+                ),
+              );
+            });
+          },
+          decoration: InputDecoration(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(40),
             ),
-          );
-        });
-      },
-      decoration: InputDecoration(
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(40),
-        ),
-        focusedBorder: OutlineInputBorder(
-          borderSide: BorderSide(
-            color: Theme.of(context).colorScheme.inverseSurface,
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                color: Theme.of(context).colorScheme.inverseSurface,
+              ),
+              borderRadius: BorderRadius.circular(40),
+            ),
+            hintText: 'Location or Transaction Number',
+            contentPadding: const EdgeInsets.fromLTRB(12, 4, 12, 4),
+            prefixIcon: const Icon(Icons.search),
           ),
-          borderRadius: BorderRadius.circular(40),
-        ),
-        hintText: 'Location or Transaction Number',
-        contentPadding: const EdgeInsets.fromLTRB(12, 4, 12, 4),
-        prefixIcon: const Icon(Icons.search),
-      ),
+        );
+      },
     );
   }
 }
