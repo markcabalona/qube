34fe19dcc57127ffdc80b39b053f03044a243627
Mark Cabalona <markcabalona58@gmail.com>
2024-09-21 13:41:13 +0800

    #6: Implement Home page with dynamic child widget

---
+++ lib/features/transactions/presentation/pages/home_page.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:qube/core/widgets/gradient_wrapper.dart';
+import 'package:qube/core/widgets/qube_stepper_widget.dart';
+import 'package:qube/features/transactions/presentation/widgets/search_transaction_widget.dart';
+
+class QubeHomePage extends StatelessWidget {
+  const QubeHomePage({
+    super.key,
+    required this.child,
+  });
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const GradientWrapper(
+          child: Icon(
+            Icons.abc,
+            size: 60,
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: _onPressedProfile,
+            icon: CircleAvatar(
+              backgroundColor:
+                  Theme.of(context).colorScheme.surfaceContainerHighest,
+              foregroundColor: Theme.of(context).colorScheme.inverseSurface,
+              child: const Icon(
+                Icons.person_outline,
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const SizedBox(height: 12),
+          const SearchTransactionWidget(),
+          const SizedBox(height: 12),
+          QubeStepperWidget(
+            index: 1,
+            children: List.generate(
+              2,
+              (index) => Text(
+                'Step ${index + 1}',
+                style: const TextStyle(
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 12),
+          Expanded(child: child),
+        ],
+      ),
+    );
+  }
+
+  void _onPressedProfile() {}
+}
